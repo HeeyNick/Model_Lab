679a0025dbeff28b1fee2f75cc1c891991d962da
HeeyNick <nikitos15032002@gmail.com>
2023-02-09 02:50:13 +0700

    Add calculation_interval and make_interval functions

--- 0lab/0lab.py
+++ 0lab/0lab.py
@@ -6,6 +6,7 @@ import numpy as np
 import plotly.io as pio
 import plotly.express as px
 import plotly.graph_objs as go
+import math
 
 import pandas as pd
 
@@ -40,25 +41,62 @@ def convert_dict_to_numpy(collections_of_values: dict):
     numpy_array = np.array(data)
     return numpy_array
 
+
+
 def plotting_discrate_population(collections_of_values: dict) -> None:
 
     plt.bar(list(collections_of_values.keys()), \
-            collections_of_values.values(), width=0.005, color='g')
+            list(collections_of_values.values()), width=0.005, color='g')
     plt.show()
 
     # fig = px.scatter(x=collections_of_values.keys(), y=collections_of_values.values())
     # fig.show()
 
+def calculated_interval_of_population(collections_of_values: dict, N: int) -> float:
 
-def make_interval_population() -> None:
-    ...
+    x_max = max(collections_of_values.keys())
+    x_min = min(collections_of_values.keys())
+
+    print(x_min, x_max)
+
+    k = (math.log10(N) * 3.322) + 1
+
+    if k > 15:
+        k = 15
+    elif k < 5:
+        k = 5
+
+    h = (x_max - x_min)/k
+
+    return k
+
+def make_intervals_population(list_of_values: list, interval: int):
+    s = pd.Series(list_of_values)
+    # s = s.groupby(pd.cut(s, bins = interval), observed= True)\
+    #           .apply(lambda x: x.to_list())
+    s = pd.cut(s, bins=interval, right=False).value_counts()
+    print(s)
+    print(type(s))
+    
 
 
 
-list_of_values = generate_list_of_vlues(N)
-collections_of_values = make_collection_of_values(list_of_values)
+def main() -> None:
 
-beautiful_print_of_discrate_population(dict(collections_of_values))
-print(collections_of_values.most_common(3))
+    list_of_values = generate_list_of_vlues(N)
+    collections_of_values = make_collection_of_values(list_of_values)
 
-plotting_discrate_population(dict(collections_of_values))
+    # beautiful_print_of_discrate_population(dict(collections_of_values))
+    # print(collections_of_values.most_common(3))
+
+    # plotting_discrate_population(dict(collections_of_values))
+
+    interval = calculated_interval_of_population(collections_of_values, N)
+    make_intervals_population(list_of_values, int(interval))
+
+
+
+
+
+if __name__ == "__main__":
+    main()
